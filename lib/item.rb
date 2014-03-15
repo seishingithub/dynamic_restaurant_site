ccfd79cd447f4fb18600b02774c7237ca9400d40
Peggy Griffin <seishingithub@gmail.com>
2014-03-15 12:00:47 -0600

    Adding item.rb file so server starts

---
+++ lib/item.rb
@@ -0,0 +1,24 @@
+class Item
+  def initialize(name, price, description, image)
+    @name = name
+    @price = price
+    @description = description
+    @image = image
+  end
+
+  def name
+    @name
+  end
+
+  def price
+    @price
+  end
+
+  def description
+    @description
+  end
+
+  def image
+    @image
+  end
+end
