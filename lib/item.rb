4624e6265207b331441631a716290536e8336d1a
Peggy Griffin <seishingithub@gmail.com>
2014-03-15 14:17:22 -0600

    created varying menu items & got restaurant site to load

--- lib/item.rb
+++ lib/item.rb
@@ -1,11 +1,12 @@
 class Item
-  def initialize(name, price, description, image)
-    @name = name
+  def initialize(name, price, description, image)  # item params
+    @name = name  # @name is variable
     @price = price
     @description = description
     @image = image
   end
 
+  # since values in vars can't be seen outside of the class Item, call method to return value of vars (e.g. on index.html)
   def name
     @name
   end
